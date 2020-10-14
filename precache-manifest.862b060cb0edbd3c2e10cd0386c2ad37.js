1b3d65345b8f54b8b837e966cfae5b55722e0768
Ruoyi Qiu <qry@RuoyideMacBook-Pro.local>
2020-10-15 03:56:30 +1100

    publish

---
+++ precache-manifest.862b060cb0edbd3c2e10cd0386c2ad37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3e39ff2d5cad2ee10df993e8016f6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df9d669a21bcce5764c",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "b882fffbb15208c1849d",
+    "url": "/static/css/main.111e333d.chunk.css"
+  },
+  {
+    "revision": "2df9d669a21bcce5764c",
+    "url": "/static/js/2.3409d495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3409d495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b882fffbb15208c1849d",
+    "url": "/static/js/main.2172869d.chunk.js"
+  },
+  {
+    "revision": "055d3969209ca2ece7be",
+    "url": "/static/js/runtime-main.6a06e15e.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
